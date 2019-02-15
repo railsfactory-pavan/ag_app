35ac2fa199c59557f8e99c524db97e1820d4e94a
railsfactory-pavan <pavanrf07@gmail.com>
2019-02-15 17:16:17 +0530

    something gets done

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,18 @@ module AgApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.raise_delivery_errors = true
+    ActionMailer::Base.smtp_settings = {
+      :address => 'smtp.gmail.com',
+      :domain => 'mail.google.com',
+      :port => 547,
+      :user_name => 'patilpavan2589@gmail.com',
+      :password => '9689409843',
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
   end
 end
