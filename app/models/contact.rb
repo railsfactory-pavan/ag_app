af3af16cce3650bae9b907bafe8316f69c77c463
railsfactory-pavan <pavanrf07@gmail.com>
2019-02-15 20:07:09 +0530

    relation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,3 @@
 class Contact < ApplicationRecord
-  
+  has_many :phone_nos
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,5 @@
 class Vehicle < ApplicationRecord
-  
+  has_many :parent_categories
+  has_many :sub_categories
+  has_many :image_galleries
 end
