c1577a7b3c127179a1f2f535bd2cd2a3538fb1ce
railsfactory-pavan <pavanrf07@gmail.com>
2019-02-16 08:16:24 +0530

    vehicle image ready at front end

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -6,4 +6,6 @@ class Vehicle < ApplicationRecord
   accepts_nested_attributes_for :parent_categories
   accepts_nested_attributes_for :sub_categories
   accepts_nested_attributes_for :image_galleries
+
+  has_one_attached :vehicle_image
 end
