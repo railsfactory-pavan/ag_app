da8689c6ed07ccae46c20e81524809c7f21dd674
railsfactory-pavan <pavanrf07@gmail.com>
2019-02-16 07:44:18 +0530

    has_many :tables, :dependent => :destroy

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,7 +1,7 @@
 class Vehicle < ApplicationRecord
-  has_many :parent_categories
-  has_many :sub_categories
-  has_many :image_galleries
+  has_many :parent_categories, :dependent => :destroy
+  has_many :sub_categories, :dependent => :destroy
+  has_many :image_galleries, :dependent => :destroy
 
   accepts_nested_attributes_for :parent_categories
   accepts_nested_attributes_for :sub_categories
